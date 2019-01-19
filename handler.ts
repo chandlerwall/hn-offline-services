cb15a9046b44a35740b628653f2004286e296c9a
Chandler Wall <c@swall.us>
2019-01-18 21:31:40 -0600

    Syntax correction and package-lock.json

--- handler.ts
+++ handler.ts
@@ -6,6 +6,7 @@ export const hello: APIGatewayProxyHandler = async (event, context) => {
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
       input: event,
+      context: context
     }),
   };
 }
