e5785ef9ed5a026bcb81340823a5fe74947dae52
Chandler Wall <c@swall.us>
2019-01-19 15:40:29 -0600

    Reformat files

--- src/hn-api.ts
+++ src/hn-api.ts
@@ -1,36 +1,36 @@
 import {APIGatewayProxyResult} from "aws-lambda";
 import firebase from "firebase/app";
 import "firebase/database";
+import App = firebase.app.App;
 import DataSnapshot = firebase.database.DataSnapshot;
 import Reference = firebase.database.Reference;
-import App = firebase.app.App;
 
 const API_URL = "https://hacker-news.firebaseio.com";
 const API_VERSION = "/v0";
 
 export async function hn(): Promise<APIGatewayProxyResult> {
-    let app: App;
-    let data: any;
+  let app: App;
+  let data: any;
 
-    try {
-        // TODO - Look for an existing Firebase application before creating a new one.
-        app = firebase.initializeApp({databaseURL: API_URL});
-        const db: Reference = app.database().ref(API_VERSION);
-        const path = "item/192327";
-        const item = db.child(path);
-        const snapshot: DataSnapshot = await item.once("value");
-        data = snapshot.val();
-    } catch (error) {
-        console.error(error);
-    } finally {
-        if (app) {
-            // TODO - Identify a more appropriate way to manage Firebase resources/applications.
-            await app.delete();
-        }
+  try {
+    // TODO - Look for an existing Firebase application before creating a new one.
+    app = firebase.initializeApp({databaseURL: API_URL});
+    const db: Reference = app.database().ref(API_VERSION);
+    const path = "item/192327";
+    const item = db.child(path);
+    const snapshot: DataSnapshot = await item.once("value");
+    data = snapshot.val();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    if (app) {
+      // TODO - Identify a more appropriate way to manage Firebase resources/applications.
+      await app.delete();
     }
+  }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
 }
