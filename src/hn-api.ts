030f617f254596fbd71909af12f54577555e6acb
Chandler Wall <c@swall.us>
2019-01-19 12:01:02 -0600

    Hacked code to find issues with promises

--- src/hn-api.ts
+++ src/hn-api.ts
@@ -7,15 +7,17 @@ const API_URL = "https://hacker-news.firebaseio.com";
 const API_VERSION = "/v0";
 
 export async function hn(): Promise<APIGatewayProxyResult> {
+    console.log("starting");
     const app = firebase.initializeApp({databaseURL: API_URL});
     const db = app.database().ref(API_VERSION);
     // console.dir(db);
-    const path = "item/8863";
+    const path = "item/192327";
     // const promise = new Promise(
     //     (resolve: (value: any) => void, reject: (reason: any) => void): void => {
     //         db.child(path).once(
     //             "value",
     //             (snapshot: DataSnapshot) => {
+    //                 console.log(snapshot);
     //                 resolve(snapshot.val());
     //             },
     //             reject
@@ -24,11 +26,12 @@ export async function hn(): Promise<APIGatewayProxyResult> {
     // );
     const item = db.child(path);
     // console.dir(item);
+    console.log("fetching");
     const snapshot = await item.once("value");
     // console.dir(snapshot.exists());
-    // const data = snapshot.val();
-    // app.delete();
     const data = snapshot.val();
+    console.dir(data);
+    await app.delete();
     return {
         statusCode: 200,
         body: JSON.stringify({
