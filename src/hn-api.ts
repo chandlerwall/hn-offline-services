48bb935961a74283c541267c9961eb35fb0e1ec2
Chandler Wall <c@swall.us>
2019-01-19 18:40:44 -0600

    Groundwork for dynamic API endpoint

--- src/hn-api.ts
+++ src/hn-api.ts
@@ -1,23 +1,27 @@
-import {APIGatewayProxyResult} from "aws-lambda";
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
 import firebase from "firebase/app";
 import "firebase/database";
 import App = firebase.app.App;
+import Database = firebase.database.Database;
 import DataSnapshot = firebase.database.DataSnapshot;
-import Reference = firebase.database.Reference;
 
 const API_URL = "https://hacker-news.firebaseio.com";
 const API_VERSION = "/v0";
 
-export async function hn(): Promise<APIGatewayProxyResult> {
+export async function hn(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+  console.log(event);
+
+  const id = event.pathParameters["id"];
+
   let app: App;
   let data: any;
 
   try {
     // TODO - Look for an existing Firebase application before creating a new one.
     app = firebase.initializeApp({databaseURL: API_URL});
-    const db: Reference = app.database().ref(API_VERSION);
-    const path = "item/192327";
-    const item = db.child(path);
+    const db: Database = app.database();
+    const path = `item/${id}`;
+    const item = db.ref(API_VERSION).child(path);
     const snapshot: DataSnapshot = await item.once("value");
     data = snapshot.val();
   } catch (error) {
@@ -31,6 +35,7 @@ export async function hn(): Promise<APIGatewayProxyResult> {
 
   return {
     statusCode: 200,
+    headers: {},
     body: JSON.stringify(data),
   };
 }
