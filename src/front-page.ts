029870abc1e77ab63dff389d83ae6c60111c48e3
Chandler Wall <c@swall.us>
2019-01-19 23:53:17 -0600

    Initial API endpoint for front_page (#3)
    * Define endpoint for /api/v1/front_page
    
    * Brute force approach to fetch front_page content
    
    * Specify more appropriate import statements

---
+++ src/front-page.ts
@@ -0,0 +1,30 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import firebase from "firebase/app";
+import "firebase/database";
+import {API_URL, fetchItemById} from "./hn-api";
+import App = firebase.app.App;
+
+export async function frontPage(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+  let app: App;
+  let items: any;
+
+  try {
+    // TODO - Look for an existing Firebase application before creating a new one.
+    app = firebase.initializeApp({databaseURL: API_URL});
+    const data = await fetchItemById(app, "topstories");
+    items = await Promise.all(data.item.slice(0, 30).map((itemId: any) => fetchItemById(app, `item/${itemId}`)));
+  } catch (error) {
+    console.error(error);
+  } finally {
+    if (app) {
+      // TODO - Identify a more appropriate way to manage Firebase resources/applications.
+      await app.delete();
+    }
+  }
+
+  return {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify(items),
+  };
+}
--- src/hn-api.ts
+++ src/hn-api.ts
@@ -5,8 +5,8 @@ import App = firebase.app.App;
 import Database = firebase.database.Database;
 import DataSnapshot = firebase.database.DataSnapshot;
 
-const API_URL = "https://hacker-news.firebaseio.com";
-const API_VERSION = "/v0";
+export const API_URL = "https://hacker-news.firebaseio.com";
+export const API_VERSION = "/v0";
 
 export async function hn(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
   let id;
@@ -47,7 +47,7 @@ export async function hn(event: APIGatewayProxyEvent): Promise<APIGatewayProxyRe
   try {
     // TODO - Look for an existing Firebase application before creating a new one.
     app = firebase.initializeApp({databaseURL: API_URL});
-    data = await fetchItemById(app, id);
+    data = await fetchItemById(app, `item/${id}`);
   } catch (error) {
     console.error(error);
   } finally {
@@ -64,24 +64,25 @@ export async function hn(event: APIGatewayProxyEvent): Promise<APIGatewayProxyRe
   };
 }
 
-async function fetchItemById(firebaseApp: App, id: any) {
+export async function fetchItemById(firebaseApp: App, path: any) {
+  // TODO - Move this function into a shared location.
+  // TODO - Improve logging to include meaningful, diagnostic information.
   const db: Database = firebaseApp.database();
-  const path = `item/${id}`;
   const itemReference = db.ref(API_VERSION).child(path);
-  console.log(`Start fetching ${id}`);
+  // console.log(`Start fetching ${path}`);
   const itemSnapshot: DataSnapshot = await itemReference.once("value");
   // TODO - Check if the requested reference exists. Return 404 if the requested item does not exist.
   // TODO - Create a type declaration for an item.
   const item = itemSnapshot.val();
-  console.log(`Finish fetching ${id}`);
+  // console.log(`Finish fetching ${path}`);
 
   let comments: object[] = [];
   if (item && item.kids && item.kids.length) {
     comments = await Promise.all(item.kids.map((kid: any) => {
-      console.log(`Prepare to fetch kid ${kid}`);
+      // console.log(`Prepare to fetch kid ${kid}`);
       // TODO - Is there a better way than recursion?
-      const kidItem = fetchItemById(firebaseApp, kid);
-      console.log(`Finish fetching kid ${kid}`);
+      const kidItem = fetchItemById(firebaseApp, `item/${kid}`);
+      // console.log(`Finish fetching kid ${kid}`);
       return kidItem;
     }));
   }
