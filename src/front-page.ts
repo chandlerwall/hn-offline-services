186be7f44e888e910d26bae692d5e8186fa1882d
Chandler Wall <c@swall.us>
2019-01-19 23:47:25 -0600

    Specify more appropriate import statements

--- src/front-page.ts
+++ src/front-page.ts
@@ -1,5 +1,6 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/database";
 import {API_URL, fetchItemById} from "./hn-api";
 import App = firebase.app.App;
 
